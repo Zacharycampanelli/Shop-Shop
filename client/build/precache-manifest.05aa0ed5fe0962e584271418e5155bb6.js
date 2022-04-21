2ea392807e9314959f7f27a3deb06e134177b102
Zachary Campanelli <84996750+Zacharycampanelli@users.noreply.github.com>
2022-04-21 00:09:11 -0400

    Added stripe to do transactions

---
+++ client/build/precache-manifest.05aa0ed5fe0962e584271418e5155bb6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "840ae4733dca36def458745497ef0647",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b39adb6053f60268a7a",
+    "url": "/static/css/main.e3b24089.chunk.css"
+  },
+  {
+    "revision": "24c40cc5354616da34f8",
+    "url": "/static/js/2.9a5f0e5c.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.9a5f0e5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b39adb6053f60268a7a",
+    "url": "/static/js/main.a5fb4fd4.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
